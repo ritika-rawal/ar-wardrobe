476aabd1e93edb6110868705f0921fbf4d4ba2d8
Ritika <1ritikarr@gmail.com>
2026-07-08 20:31:26 +0545

    changed url

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({ baseURL: '/api' });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token'); 
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
